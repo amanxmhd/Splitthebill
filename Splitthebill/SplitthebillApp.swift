ef13dd23e59b21aee001d9e1c91eb31e9e44f5f1
Aman <amanxmhd@gmail.com>
2024-08-31 16:17:43 +0530

    Initial Commit

---
+++ Splitthebill/SplitthebillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitthebillApp.swift
+//  Splitthebill
+//
+//  Created by Aman on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitthebillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
